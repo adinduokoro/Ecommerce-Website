ee2725bf17a9a0dd2bb88c024db4c52ebe1648f7
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-10-02 12:13:37 -0400

    Nav/Header Section
    Finished creating a sliding notification UI. Working on building the remainder of the header and navbar section.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,12 @@ const modalCloseFunc = function(){
 //modal eventListener
 modalCloseOverlay.addEventListener('click', modalCloseFunc)
 modalCloseBtn.addEventListener('click', modalCloseFunc)
+
+//notification toast variables
+const notificationToast = document.querySelector('[data-toast]')
+const toastCloseBtn = document.querySelector('[data-toast-close]')
+
+//notification toast eventListener
+toastCloseBtn.addEventListener('click', function(){
+  notificationToast.classList.add('closed')
+})
